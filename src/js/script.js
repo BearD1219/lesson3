297f77b7c9c24bbcae38d0190bd57205b99d5cd3
Denis <denis.m3927@gmail.com>
2021-10-04 14:37:04 +0200

    add up-page link

--- src/js/script.js
+++ src/js/script.js
@@ -119,3 +119,18 @@ $('form').submit(function(e) {
     });
     return false;
 });
+
+// SMOOTH SCROLL
+$(window).scroll(function() {
+    if($(this).scrollTop() > 1600) {
+        $(".page-up").fadeIn();
+    } else {
+        $(".page-up").fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
