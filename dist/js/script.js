8c987c595eb2f3b8b2aa6efe52b8c5d81a9a2635
Denis <denis.m3927@gmail.com>
2021-10-05 14:23:25 +0200

    FINISH

---
+++ dist/js/script.js
@@ -0,0 +1,139 @@
+// SWIPER
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+});
+
+// TABS
+$(function() {    
+  $('.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this).addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active');
+    $(this).closest('.container').find('.catalog__wrap').removeClass('catalog__wrap_active').eq($(this).index()).addClass('catalog__wrap_active');
+  });
+});
+
+// CARDS
+function toggleSlide (item) {
+  $(item).each(function(i) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('.catalog__item-content').eq(i).toggleClass('catalog__item-content_active');
+      $('.catalog__item-descr').eq(i).toggleClass('catalog__item-descr_active');
+    });
+  });
+}
+
+toggleSlide('.catalog__link');
+toggleSlide('.catalog__link-back');
+
+// MODALS
+// const buttons = document.querySelectorAll('[data-modal]');
+// const overlay = document.querySelector('.overlay');
+// const closeButtons = document.querySelectorAll('.modal__close');
+// let currentModal;
+
+// for (let button of buttons) {
+//     button.addEventListener('click', () => {
+//         overlay.classList.add('overlay_active');
+//         currentModal = document.querySelector(button.getAttribute('data-modal'));
+//         currentModal.style.display = "block";
+
+//         for (let closeButton of closeButtons) {
+//             document.addEventListener('click', (e) => {
+//                 if (e.target == overlay || closeButton) {
+//                     currentModal.style.display = "none";
+//                     overlay.classList.remove('overlay_active');
+//                 } else {
+//                     console.log('ЖОПА');
+//                 }
+//             });
+//         }
+//     });
+// } 
+$('[data-modal]').on('click', (e) => {
+    let modalCurr = $(e.target).data('modal');
+    let parent = $(e.target).parent().siblings()[0];
+    let itemName = $(parent).find('.catalog__name').text();
+    $(`${modalCurr}, .overlay`).fadeIn('slow');
+
+    if(modalCurr == "#order") {
+        $(`${modalCurr} > .modal__subtitle`).text(itemName);
+    }
+});
+
+$('.modal__close').on('click', () => {
+    $('.overlay, #consultation, #order, #info').fadeOut('slow');
+});
+
+// VALIDATION 
+function validateForm(form) {
+    $(form).validate({
+        rules: {
+            name: "required",
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: "Обязательное поле для заполнения",
+            phone: "Обязательное поле для заполнения",
+            email: {
+                required: "Обязательное поле для заполнения",
+                email: "Некорректный почтовый адрес"
+            }
+        }
+    });
+}
+
+validateForm('.consultation form');
+validateForm('#consultation form');
+validateForm('#order form');
+
+// TELEPHONE MASK
+$("[name = phone]").mask("+7 (999) 999-99-99");
+
+// MAILER
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find('input').val('');
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #info').fadeIn('slow');
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+// SMOOTH SCROLL
+$(window).scroll(function() {
+    if($(this).scrollTop() > 1000) {
+        $(".page-up").fadeIn();
+    } else {
+        $(".page-up").fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+// Animate
+new WOW().init();
--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,21 @@ const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+
+
 
 // Static server
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         },
         notify: false
     });
+
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
@@ -26,14 +32,30 @@ gulp.task('styles', function() {
             suffix: ".min",
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('src/css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
+});
+
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('onceMoveFiles', function() {
+    return gulp.src(['src/mailer/**', 'src/fonts/**', 'src/img/**'], {base: 'src/'})
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('src/js/**/*.js', {base: 'src/'})
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
     gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
-    gulp.watch('src/*.html').on('change', browserSync.reload);
-    gulp.watch('src/js/*.js').on('change', browserSync.reload);
+    gulp.watch('src/*.html').on('change', gulp.parallel('html'));
+    gulp.watch('src/js/**/*.js').on('change', gulp.parallel('scripts'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'onceMoveFiles', 'scripts'));
